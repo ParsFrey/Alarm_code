1c2bb942df260654069e7b4536e7203e7f933b39
ParsFrey <153170465+ParsFrey@users.noreply.github.com>
2024-05-27 00:41:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ stop = False
 while stop == False:
     rn = str(datetime.datetime.now().time())
     print(rn)
-    x = int(datetime.datetime.now().time() + '00:00:10.000000')
-    if rn > x:
+    if rn > '17:00:0.000000':
         stop = True
         print("OK")
         break
         #os.system("ring.mp3")
+
 
 
         
